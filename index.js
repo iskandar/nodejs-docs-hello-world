7fc08328dac36e17e1fff2ab9eb77934fc618112
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2018-03-07 21:35:48 +0000

    Start server in properties parse() callback

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 var http = require('http');
 var _ = require('lodash');
-var properties = require ("properties");
- 
+var properties = require("properties");
+
 var buildProperties = { oops: "not found" };
-properties.parse ("build.properties", { path: true }, function (error, obj){
-    if (error) return console.error (error);
-    buildProperties = obj;
-  });
+properties.parse("build.properties", { path: true }, startServer);
 
-var server = http.createServer(function(request, response) {
+function startServer(error, props) {
+    if (error) return console.error(error);
+    buildProperties = props;
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("<h1>Hello World!</h1>");
-    response.write("<h2>Build Properties</h2>");
-    response.write("<textarea style='width: 100%; height: 300px;'>" + JSON.stringify(buildProperties, null, 2) + "</textarea>");
-    response.write("<h2>Env Vars</h2>");
-    response.write("<textarea style='width: 100%; height: 600px;'>" + JSON.stringify(process.env, null, 2) + "</textarea>");
-    response.end();
-});
+    var server = http.createServer(function (request, response) {
 
-var port = process.env.PORT || 1337;
+        response.writeHead(200, { "Content-Type": "text/html" });
+        response.write("<h1>Hello World!</h1>");
+        response.write("<h2>Build Properties</h2>");
+        response.write("<textarea style='width: 100%; height: 300px;'>" + JSON.stringify(buildProperties, null, 2) + "</textarea>");
+        response.write("<h2>Env Vars</h2>");
+        response.write("<textarea style='width: 100%; height: 600px;'>" + JSON.stringify(process.env, null, 2) + "</textarea>");
+        response.end();
+    });
 
-// Dump ALL of the environment vars
-console.log("env config", process.env);
+    var port = process.env.PORT || 1337;
 
-server.listen(port);
+    // Dump ALL of the environment vars
+    console.log("env config", process.env);
 
-console.log("Server running at http://localhost:%d", port);
+    server.listen(port);
+
+    console.log("Server running at http://localhost:%d", port);
+}
