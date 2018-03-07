88268e1e44627e15954333b59127c609362414fe
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2018-03-07 20:51:47 +0000

    Add env vars to index page

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ var http = require('http');
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/html"});
-    response.end("<h1>Hello World!</h1>");
-
+    response.write("<h1>Hello World!</h1>");
+    response.write("<h2>Env vars:</h2>");
+    response.write("<textarea style='width: 100%; height: 600px;'>" + JSON.stringify(process.env, null, 2) + "</textarea>");
+    response.end();
 });
 
 var port = process.env.PORT || 1337;
