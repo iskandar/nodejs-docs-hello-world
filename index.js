c82002c80ebe032ae7b631762833f6d7c72129c1
Iskandar Najmuddin <iskandar.najmuddin@rackspace.co.uk>
2018-03-07 21:09:54 +0000

    Add build properties to index

--- index.js
+++ index.js
@@ -1,11 +1,20 @@
 var http = require('http');
 var _ = require('lodash');
+var properties = require ("properties");
+ 
+var buildProperties = { oops: "not found" };
+properties.parse ("build.properties", { path: true }, function (error, obj){
+    if (error) return console.error (error);
+    buildProperties = obj;
+  });
 
 var server = http.createServer(function(request, response) {
 
     response.writeHead(200, {"Content-Type": "text/html"});
     response.write("<h1>Hello World!</h1>");
-    response.write("<h2>Env vars:</h2>");
+    response.write("<h2>Build Properties</h2>");
+    response.write("<textarea style='width: 100%; height: 300px;'>" + JSON.stringify(buildProperties, null, 2) + "</textarea>");
+    response.write("<h2>Env Vars</h2>");
     response.write("<textarea style='width: 100%; height: 600px;'>" + JSON.stringify(process.env, null, 2) + "</textarea>");
     response.end();
 });
